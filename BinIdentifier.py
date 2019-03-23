67128a0666cef97a4544079604b526c8ed57f324
sluder <zachary.sluder@gmail.com>
2019-03-23 19:32:26 -0400

    fv for bins

--- BinIdentifier.py
+++ BinIdentifier.py
@@ -2,6 +2,7 @@
 
 import r2pipe
 import os
+from collections import OrderedDict
 
 rom_id_locations = {
     'eg33' : '0x8c3d',
@@ -12,6 +13,8 @@ rom_id_locations = {
     'ej22' : '0x8ddc'
 }
 
+clusters = {}
+
 class EcuFile:
     def __init__(self, file_name):
         r2 = r2pipe.open('./bins/' + file_name)
@@ -20,18 +23,48 @@ class EcuFile:
         r2.cmd('e anal.from=0x9000')
         r2.cmd('e anal.to=0xffff')
 
-        for bin_type, location in rom_id_locations.items():
-            r2.cmd('s {}'.format(location))
+        self.get_id_location(r2)
+        self.get_fvector_location(r2)
+        self.get_8000_data(r2)
 
-            if r2.cmd('px0')[:1] == '7':
-                self.bin_type = bin_type
-                break
         r2.quit()
 
+    def get_id_location(self, r2):
+        for bin_type, location in rom_id_locations.items():
+            r2.cmd('s {}'.format(location))
+            id = r2.cmd('px0')
+
+            if id[:1] == '7':
+                self.rom_id = id[:6]
+                self.bin_type = bin_type
+                self.id_location = location
+                break
+
+    def get_fvector_location(self, r2):
+        r2.cmd('s 0xfffe')
+        location = r2.cmd('px0')[:4]
+
+        self.fvector_location = location[2:4] + location[:2]
+
+    def get_8000_data(self, r2):
+        r2.cmd('s 0x8000')
+        self.data_0x8000 = r2.cmd('px0')[:2]
+
+    def to_string(self):
+        return 'Type: {} .. ID: {} .. ID L: {} .. 0x8000: {}'.format(self.bin_type, self.rom_id, self.id_location, self.data_0x8000)
 
 if __name__ == '__main__':
     ecu_files = {}
 
     for filename in os.listdir('./bins'):
-        ecu_files[filename] = EcuFile(filename)
-        # ecu_files[filename].to_string()
+        ecu = EcuFile(filename)
+
+        if ecu.fvector_location not in clusters:
+            clusters[ecu.fvector_location] = []
+        clusters[ecu.fvector_location].append(ecu)
+
+    for address, ecus in clusters.items():
+        print(address)
+
+        for ecu in ecus:
+            print('\t' + ecu.to_string())
