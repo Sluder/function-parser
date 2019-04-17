201c0a5fc3728ff37aeb63d661040b524300be3c
sluder <zachary.sluder@gmail.com>
2019-04-16 21:06:40 -0400

    fvector analysis

--- BinIdentifier.py
+++ BinIdentifier.py
@@ -2,6 +2,7 @@
 
 import r2pipe
 import os
+import json
 from collections import OrderedDict
 
 rom_id_locations = {
@@ -20,12 +21,13 @@ class EcuFile:
         r2 = r2pipe.open('./bins/' + file_name)
         r2.cmd('e asm.arch=m7700')
         r2.cmd('e anal.limits=true')
-        r2.cmd('e anal.from=0x9000')
-        r2.cmd('e anal.to=0xffff')
+        r2.cmd('e anal.from=0x8000')
+        r2.cmd('e anal.to=0xfffe')
 
         self.get_id_location(r2)
         self.get_fvector_location(r2)
-        self.get_8000_data(r2)
+        self.get_fvector_calls(r2)
+        #self.get_8000_data(r2)
 
         r2.quit()
 
@@ -50,13 +52,51 @@ class EcuFile:
         r2.cmd('s 0x8000')
         self.data_0x8000 = r2.cmd('px0')[:2]
 
-    def to_string(self):
-        return 'Type: {} .. ID: {} .. ID L: {} .. 0x8000: {}'.format(self.bin_type, self.rom_id, self.id_location, self.data_0x8000)
+    def get_fvector_calls(self, r2):
+        r2.cmd('s 0x{}'.format(self.fvector_location))
+        r2.cmd('aaa')
+
+        instructions = json.loads(r2.cmd('pdj 1000').replace('\r\n', '').decode('utf-8', 'ignore'), strict=False)
+        # instructions = instructions['ops']
+        calls = []
+        stores = 0
+        watch = False
+        self.healthcheck = 0x0000
+
+        for i, ins in enumerate(instructions):
+            split = ins['opcode'].split(' ')
+
+            if not watch:
+                if split[0] == 'STA':
+                    stores += 1
+                else:
+                    stores = 0
+
+                if stores >= 6:
+                    watch = True
+            else:
+                if split[0] == 'JSR':
+                    calls.append(split)
+                elif len(calls) > 10:
+                    break
+                else:
+                    calls = []
+                    continue
+        if calls:
+            self.healthcheck = calls[0][1]
+        self.fvector_calls = len(calls)
+        self.fvector_jmps = calls
+
+    def __str__(self):
+        return 'Type: {} .. RomID: {} .. IDLocation: {} .. FVCalls : {} .. HC : {}'.format(self.bin_type, self.rom_id, self.id_location, self.fvector_calls, self.healthcheck)
 
 if __name__ == '__main__':
     ecu_files = {}
 
     for filename in os.listdir('./bins'):
+        # if filename != '733257-1993-Canada-Legacy-EJ22.bin':
+        #     continue
+        print(filename)
         ecu = EcuFile(filename)
 
         if ecu.fvector_location not in clusters:
@@ -67,4 +107,4 @@ if __name__ == '__main__':
         print(address)
 
         for ecu in ecus:
-            print('\t' + ecu.to_string())
+            print('\t' + str(ecu))
